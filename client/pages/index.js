8b74e40828e72ef5fa3b2ebf1bb1f2c86a0c5964
mhaisam350 <mhaisam350@gmail.com>
2022-12-26 21:51:10 +0500

    Set up env file and installed shopify-buy

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,24 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
+
   return (
-    <div className={styles.container}>
-      
-    </div>
+
+    <>
+
+      <Head>
+        <title>Eon</title>
+      </Head>
+
+      <main>
+
+        <h1>Shop</h1>
+
+      </main>
+
+    </>
+
   )
+  
 }
